a28d6e38b2024937399ef0ab2e97a45bbaf61637
Sarang <hangpfm0518@ewhain.net>
2024-10-28 15:37:04 +0900

    Dice ~ step3까지 완성

--- app/index.tsx
+++ app/index.tsx
@@ -1,52 +1,105 @@
-import { Text, View, Button, TextInput } from "react-native";
+import { Text, View, Button, StyleSheet } from "react-native";
 import { useState } from "react";
 import React from "react";
 
-var input_st = { fontSize: 20, borderWidth: 1, borderColor: 'gray', flex: 1, padding: 5, margin: 2 };
+var st_circle = {
+  width: 30,
+  height: 30,
+  borderRadius: 20,
+  backgroundColor: 'rgb(0, 200, 255)', 
+  margin: 2
+};
 
-var N = ['Ewha', 'June', 'Jane', "Nick"];
-var P = [1234, 3344, 1111, 1212];
+var st_dice = {
+  backgroundColor: 'rgb(255, 240, 200)',
+  padding: 10
+};
 
-export default function App() {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [refresh, setRefresh] = useState(0);
+function Circle() {
+  return <View style={st_circle} />;
+}
 
-  function add_item() {
-    N.push(name);
-    P.push(Number(phone));
-    setRefresh(refresh + 1);
-  }
+function Blank() {
+  return <View style={[st_circle, {backgroundColor: 'rgb(255, 240, 200)', borderWidth: 0}]} />;
+}
 
-  var L = [];
+interface DiceProps {
+  num: number;
+}
 
-  for (var i = 0; i < N.length; i++) {
-    var a = (
-      <View key={i} style={{ flexDirection: 'row', marginBottom: 5 }}>
-        <TextInput 
-          style={[input_st, { marginRight: 5 }]} 
-          value={N[i]} 
-          editable={false} 
-        />
-        <TextInput 
-          style={input_st} 
-          value={P[i].toString()} 
-          editable={false} 
-        />
+function Dice(props: DiceProps) {
+  if(props.num == 1){
+    return (
+      <View style={st_dice}>
+        <View style={{flexDirection: 'row'}}><Blank /><Blank /><Blank /></View>
+        <View style={{flexDirection: 'row'}}><Blank /><Circle /><Blank /></View>
+        <View style={{flexDirection: 'row'}}><Blank /><Blank /><Blank /></View>
       </View>
     );
-    L.push(a);
+  } else if(props.num == 2){
+    return (
+      <View style={st_dice}>
+        <View style={{flexDirection: 'row'}}><Circle /><Blank /><Blank /></View>
+        <View style={{flexDirection: 'row'}}><Blank /><Blank /><Blank /></View>
+        <View style={{flexDirection: 'row'}}><Blank /><Blank /><Circle /></View>
+      </View>
+    );
+  } else if(props.num == 3){
+    return (
+      <View style={st_dice}>
+        <View style={{flexDirection: 'row'}}><Circle /><Blank /><Blank /></View>
+        <View style={{flexDirection: 'row'}}><Blank /><Circle /><Blank /></View>
+        <View style={{flexDirection: 'row'}}><Blank /><Blank /><Circle /></View>
+      </View>
+    );
+  } else if(props.num == 4){
+    return (
+      <View style={st_dice}>
+        <View style={{flexDirection: 'row'}}><Circle /><Blank /><Circle /></View>
+        <View style={{flexDirection: 'row'}}><Blank /><Blank /><Blank /></View>
+        <View style={{flexDirection: 'row'}}><Circle /><Blank /><Circle /></View>
+      </View>
+    );
+  } else if(props.num == 5){
+    return (
+      <View style={st_dice}>
+        <View style={{flexDirection: 'row'}}><Circle /><Blank /><Circle /></View>
+        <View style={{flexDirection: 'row'}}><Blank /><Circle /><Blank /></View>
+        <View style={{flexDirection: 'row'}}><Circle /><Blank /><Circle /></View>
+      </View>
+    );
+  } else {
+    return (
+      <View style={st_dice}>
+        <View style={{flexDirection: 'row'}}><Circle /><Blank /><Circle /></View>
+        <View style={{flexDirection: 'row'}}><Circle /><Blank /><Circle /></View>
+        <View style={{flexDirection: 'row'}}><Circle /><Blank /><Circle /></View>
+      </View>
+    );
   }
+}
+
+export default function App() {
+  const [N, setN] = useState(1)
 
   return (
-    <View>
-      <Text style={{fontSize: 40}}>Phone Book</Text>
-      <View style={{margin: 10, flexDirection: 'row'}}>
-        <TextInput style={input_st} onChangeText={setName} placeholder="Name"/>
-        <TextInput style={input_st} onChangeText={setPhone} placeholder="Phone"/>
-        <Button title="ADD" onPress={add_item}/>
-      </View>
-      {L}
+    <View style={{flex:1}}>
+      <Text style={styles.text}>Dice</Text>
+      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+      <Dice num={N} />
+    </View>
+    <View style={{marginHorizontal:100, marginVertical:30}}>
+      <Button title="Roll" onPress={
+        () => setN(Math.floor(Math.random() * 6) + 1)} />
+    </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    margin: 10,
+  },
+});
